619d18748dd056e1ff4f1358d5957e335b9c9bea
takashi388412 <take388412@gmail.com>
2020-02-25 02:38:50 +0900

    smooth scroll 実装

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 
+//slideshow
 $(function () {
     // slideshow クラスを持った要素ごとに処理を実行
-    $('.slideshow').each(function () {
+    $('.top').each(function () {
 
         var $slides = $(this).find('img'), // すべてのスライド
             slideCount = $slides.length,   // スライドの点数
@@ -11,7 +12,7 @@ $(function () {
         $slides.eq(currentIndex).fadeIn();
 
         // 7500 ミリ秒ごとに showNextSlide 関数を実行
-        setInterval(showNextSlide, 7500);
+        setInterval(showNextSlide, 2500);
 
         // 次のスライドを表示する関数
         function showNextSlide () {
@@ -31,5 +32,81 @@ $(function () {
         }
 
     });
+
+});
+
+// //sticky header
+// $(function () {
+
+//     $('.header').each(function () {
+
+//         var $window = $(window), // Window オブジェクト
+//             $header = $(this),   // ヘッダー
+
+//             // ヘッダーのクローン
+//             $headerClone = $header.contents().clone(),
+
+//             // ヘッダーのクローンのコンテナー
+//             $headerCloneContainer = $('<div class="header-clone"></div>'),
+
+//             // HTML の上辺からヘッダーの底辺までの距離 = ヘッダーのトップ位置 + ヘッダーの高さ
+//             threshold = $header.offset().top + $header.outerHeight();
+
+//         // コンテナーにヘッダーのクローンを挿入
+//         $headerCloneContainer.append($headerClone);
+
+//         // コンテナーを body の最後に挿入
+//         $headerCloneContainer.appendTo('body');
+
+//         // スクロール時に処理を実行するが、回数を 1 秒間あたり 15 までに制限
+//         $window.on('scroll', $.throttle(1000 / 15, function () {
+//             if ($window.scrollTop() > threshold) {
+//                 $headerCloneContainer.addClass('visible');
+//             } else {
+//                 $headerCloneContainer.removeClass('visible');
+//             }
+//         }));
+
+//         // スクロールイベントを発生させ、初期位置を決定
+//         $window.trigger('scroll');
+//     });
+
+// });
+
+//  smooth scroll
+$(function () {
+
+    $('.back-to-top').each(function () {
+
+        // html か body のいずれがスクロール可能な要素かを検出
+        var $el = $(scrollableElement('html', 'body'));
+
+        // ボタンにクリックイベントを設定
+        $(this).on('click', function (event) {
+            event.preventDefault();
+            $el.animate({ scrollTop: 0 }, 250);
+        });
+    });
+
+    // scrollTop が利用できる要素を検出する関数
+    // http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links#update4
+    function scrollableElement (elements) {
+        var i, len, el, $el, scrollable;
+        for (i = 0, len = arguments.length; i < len; i++) {
+            el = arguments[i],
+            $el = $(el);
+            if ($el.scrollTop() > 0) {
+                return el;
+            } else {
+                $el.scrollTop(1);
+                scrollable = $el.scrollTop() > 0;
+                $el.scrollTop(0);
+                if (scrollable) {
+                    return el;
+                }
+            }
+        }
+        return [];
+    }
 
 });
